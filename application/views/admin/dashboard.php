b02001976934cdc49ed90f06ad20f769a849227f
rafael.hernandez <rafael.hdz@comimsa.com>
2023-04-23 23:23:10 -0600

    Proyecto base

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,19 @@
+
+            <div class="content-wrapper">
+            <section class="content-header">
+        <h1>
+            <center><strong><font color="#D34787">"Escuela"</font></strong></center>
+    <center><small><font color="#2F4D97" face="Comic Sans MS,arial,verdana">Ciudad Iguala de la Independencia, Guerrero</font></small></center>
+        </h1>
+            </section>
+
+            <section class="content">
+<form>
+                <center>
+                    <img src="<?php echo base_url()?>assets/template/dist/img/logo.jpg"  class="user-image" alt="User Image" width=420px heigth=420px link="">
+                   <center>
+
+</form>
+
+            </section>
+        </div>
